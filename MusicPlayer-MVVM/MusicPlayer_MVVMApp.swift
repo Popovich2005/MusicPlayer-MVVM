8e136e82ab8078ff8e08b66c7d144ecb821b31bf
Popovich2005 <popich2005@mail.ru>
2024-06-27 12:42:43 +0300

    Initial Commit

---
+++ MusicPlayer-MVVM/MusicPlayer_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayer_MVVMApp.swift
+//  MusicPlayer-MVVM
+//
+//  Created by Алексей Попов on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayer_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
