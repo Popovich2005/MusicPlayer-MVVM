0703173a9a8279488b45e584564fca449b158654
Popovich2005 <popich2005@mail.ru>
2024-07-03 11:53:40 +0300

    create full player

---
+++ MusicPlayer-MVVM/Hellper/SizeConstant.swift
@@ -0,0 +1,13 @@
+//
+//  SizeConstant.swift
+//  MusicPlayer-MVVM
+//
+//  Created by Алексей Попов on 03.07.2024.
+//
+
+import UIKit
+
+struct SizeConstant {
+    static let miniPlayer: CGFloat = 70
+    static let fullPlayer: CGFloat = UIScreen.main.bounds.height + 250
+}
--- MusicPlayer-MVVM/Views/PlayerView.swift
+++ MusicPlayer-MVVM/Views/PlayerView.swift
@@ -11,7 +11,13 @@ struct PlayerView: View {
     
     // MARK: - Properties
     @StateObject var vm = ViewModel()
-    @State var showFiles = false
+    @State private var showFiles = false
+    @State private var showDetails = true
+    @Namespace private var playerAnimation
+    
+    var frameImage: CGFloat {
+        showDetails ? 320 : 50
+    }
     
     // MARK: - Body
     var body: some View {
@@ -19,16 +25,104 @@ struct PlayerView: View {
             ZStack {
                 BackgroundView()
                 
-                /// List of songs
-                List {
-                    ForEach(vm.songs) { song in
-                        SongCell(song: song, durationFormated: vm.durationFormated)
-                            .onTapGesture {
-                                vm.playAudio(song: song)
+                
+                VStack {
+                    
+                    /// List of songs
+                    List {
+                        ForEach(vm.songs) { song in
+                            SongCell(song: song, durationFormated: vm.durationFormated)
+                                .onTapGesture {
+                                    vm.playAudio(song: song)
+                                }
+                        }
+                    }
+                    .listStyle(.plain)
+                    
+                    Spacer()
+                    
+                    // MARK: - Player
+                    VStack {
+                        
+                        /// Mini Player
+                        HStack {
+                            Color.white
+                                .frame(width: frameImage, height: frameImage)
+                            
+                            if !showDetails {
+                                
+                                /// Description
+                                VStack(alignment: .leading) {
+                                    Text("Name")
+                                        .nameFont()
+                                    Text("Unknow Artist")
+                                        .artistFont()
+                                }
+                                .matchedGeometryEffect(id: "Description", in: playerAnimation)
+                                
+                                Spacer()
+                                
+                                CustomButton(image: "play.fill", size: .title) {
+                                    //
+                                }
                             }
+                        }
+                        .padding()
+                        .background(showDetails ? .clear : .black.opacity(0.3))
+                        .cornerRadius(10)
+                        .padding()
+                        
+                        /// Full Player
+                        if showDetails {
+                            
+                            /// Description
+                            VStack {
+                                Text("Name")
+                                    .nameFont()
+                                Text("Unknow Artist")
+                                    .artistFont()
+                            }
+                            .matchedGeometryEffect(id: "Description", in: playerAnimation)
+                            .padding(.top)
+                            
+                            VStack {
+                                /// Duration
+                                HStack {
+                                    Text("00:00")
+                                    Spacer()
+                                    Text("03:27")
+                                }
+                                .durationFont()
+                                .padding()
+                                
+                                /// Slider
+                                Divider()
+                                
+                                
+                                HStack(spacing: 40) {
+                                    CustomButton(image: "backward.end.fill", size: .title2) {
+                                        //
+                                    }
+                                    CustomButton(image: "play.circle.fill", size: .largeTitle) {
+                                        //
+                                    }
+                                    CustomButton(image: "forward.end.fill", size: .title2) {
+                                        //
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 40)
+                        }
+                    }
+                    .frame(height: showDetails 
+                           ? SizeConstant.fullPlayer
+                           : SizeConstant.miniPlayer)
+                    .onTapGesture {
+                        withAnimation(.spring) {
+                            self.showDetails.toggle()
+                        }
                     }
                 }
-                .listStyle(.plain)
             }
             
             // MARK: - Navigation Bar
@@ -48,6 +142,18 @@ struct PlayerView: View {
                 ImportFileManager(songs: $vm.songs)
             })
         }
+    }
+    
+    // MARK: - Methods
+    private func CustomButton(image: String, size: Font, action: @escaping () -> ()) -> some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: image)
+                .foregroundStyle(.white)
+                .font(size)
+        }
+        
     }
 }
 
